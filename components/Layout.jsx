bed36e44e6b83eac62b1573411b1c1fca2f8a971
ianbrdeguzman <ianbrdeguzman@gmail.com>
2021-05-31 19:52:16 -0400

    fixed overflow x

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -20,9 +20,11 @@ const Layout = ({ children }) => {
                     href='data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>💻</text></svg>'
                 ></link>
             </Head>
-            <Header />
-            {children}
-            <Footer />
+            <div className='overflow-x-hidden'>
+                <Header />
+                {children}
+                <Footer />
+            </div>
         </div>
     );
 };
