ad887270f161652901ed5e2075a3205ef5efae88
ianbrdeguzman <ianbrdeguzman@gmail.com>
2022-01-05 10:46:33 -0500

    add new project card component

--- components/ProjectSection.tsx
+++ components/ProjectSection.tsx
@@ -1,7 +1,5 @@
-import { FiGithub } from 'react-icons/fi';
-import { MdOpenInNew } from 'react-icons/md';
-import Link from 'next/link';
 import { IProjectProps } from '../lib/types';
+import ProjectCard from './ProjectCard';
 
 const ProjectSection = ({ projects }: { projects: IProjectProps[] }) => {
   return (
@@ -24,57 +22,8 @@ const ProjectSection = ({ projects }: { projects: IProjectProps[] }) => {
         </header>
         <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-3">
           {projects.map((project: IProjectProps, i: number) => {
-            const { id, name, description, technologies, repo, demo } = project;
             const delay = String((i + 1) * 100);
-            return (
-              <div
-                data-aos="fade-up"
-                data-aos-delay={delay}
-                key={id}
-                className="rounded min-h-[300px] flex flex-col border p-4"
-              >
-                <header className="flex justify-end">
-                  <Link href={repo}>
-                    <a
-                      target="_blank"
-                      className="text-black text-xl dark:text-white transform hover:scale-125 transition"
-                    >
-                      <FiGithub />
-                    </a>
-                  </Link>
-                </header>
-                <Link href={`/project/${id}`} passHref>
-                  <main className="flex-1 my-4 flex flex-col cursor-pointer">
-                    <h3 className="text-xl md:text-2xl font-normal">{name}</h3>
-                    <p className="my-4 flex-1 line-clamp-4">{description}</p>
-                    <div className="flex flex-wrap">
-                      {technologies.map((item, i) => {
-                        return (
-                          <span
-                            key={i}
-                            className="mr-1 mb-1 bg-gradient-to-r from-blue-500 via-purple-600 via-pink-500 to-red-400 rounded"
-                          >
-                            <span className="m-0.5 px-2 rounded bg-white dark:bg-gray-900 text-sm block">
-                              {item}
-                            </span>
-                          </span>
-                        );
-                      })}
-                    </div>
-                  </main>
-                </Link>
-                <footer className="flex justify-end">
-                  <Link href={demo}>
-                    <a
-                      target="_blank"
-                      className="text-black text-xl dark:text-white transform hover:scale-125 transition"
-                    >
-                      <MdOpenInNew />
-                    </a>
-                  </Link>
-                </footer>
-              </div>
-            );
+            return <ProjectCard key={project.id} delay={delay} {...project} />;
           })}
         </div>
       </div>
